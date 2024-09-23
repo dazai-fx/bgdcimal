521b7ca22d9b945df805ec096436180c06d3ea93
José Antonio <joseanlopezavila@gmail.com>
2024-09-23 19:40:13 +0200

    tratando de hacer ejercicio de pagos de prestamo amortizable (sale mal)

--- src/main/java/org/iesvdm/ejercicios/pagos_de_prestamo_amortizable_ejer3.java
+++ src/main/java/org/iesvdm/ejercicios/pagos_de_prestamo_amortizable_ejer3.java
@@ -9,47 +9,47 @@ public class pagos_de_prestamo_amortizable_ejer3 {
 
     public static void main(String[] args) {
 
-        BigDecimal p = new BigDecimal("200000");
+        BigDecimal p = new BigDecimal("200000"); // Monto original
 
-        System.out.println("Préstamo: "+p.setScale(0, RoundingMode.HALF_UP)+"€");
+        BigDecimal r = new BigDecimal("0.05"); // tasa de interés anual 0.05/12 para calcular la tasa anual
 
-        BigDecimal r = new BigDecimal("0.05"); // 0.05 / 12 para calcular la tasa anual
+        int n = 30; // Número total de pagos (en meses)
 
-        System.out.println("Tasa de interés anual: "+r.setScale(2, RoundingMode.HALF_UP)+"%");
+        // Operaciones
 
-        BigDecimal n = new BigDecimal("30");
-
-        System.out.println("Pago mensual: ");
+        // 1. Convertir la tasa de interés anual a mensual
 
         r = r.divide(new BigDecimal("12"), 10, RoundingMode.HALF_UP);
 
-        BigDecimal pagoMensual = r.add(BigDecimal.ONE);
+        System.out.println("r = "+r.setScale(10, RoundingMode.HALF_UP)+"%");
 
-        int exponent = -360;
+        // 2.Calcular el pago mensual (PMT)
+
+        BigDecimal numerador = p.multiply(r);
+
+        BigDecimal denominador = r.add(BigDecimal.ONE);
+
+        int exponente = n * -1;
 
         MathContext mc = new MathContext(10, RoundingMode.HALF_UP);
 
-        if(exponent < 0){
-            // calcular la potencia positiva
-            BigDecimal positivePower = r.pow(Math.abs(exponent), mc);
-            // tomamos el inverso de esa potencia (1/positivePower)
-            BigDecimal result = BigDecimal.ONE.divide(positivePower, mc);
-            System.out.println(result);
-        }else{
-            // calcular el exponente si no es negativo
-            BigDecimal result = r.pow(exponent, mc);
-            System.out.println(result);
-        }
+        denominador = BigDecimal.ONE.divide(denominador.pow(Math.abs(exponente), mc), mc);
 
-        BigDecimal interesPrimerMes = p.multiply(r);
+        denominador = BigDecimal.ONE.subtract(denominador);
 
+        BigDecimal pmt = numerador.divide(denominador, 2, RoundingMode.HALF_UP);
 
-        System.out.println("Interes del primer mes =  "+p+" x "+r+" = "+interesPrimerMes);
+        System.out.println("PMT = "+pmt); // el resultado no corresponde con el ejemplo dado algo esta mal (preguntar al profe)
 
+        // 3. Desglosar el pago mensual en:
+
+        //BigDecimal saldoPendiente = p;
+
+        //BigDecimal interesDelMes = saldoPendiente.multiply(r);
+
+        //System.out.println(interesDelMes.setScale(2, RoundingMode.HALF_UP)+"€");
 
 
-
-        System.out.println(p.toString());
 
     }
 
